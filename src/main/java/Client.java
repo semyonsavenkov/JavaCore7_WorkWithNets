39008d28519145f97c09cf8f614409ed0eddbfa1
semyonsavenkov <semyonsavenkov@gmail.com>
2023-04-01 14:44:36 +0300

    little changes

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -15,7 +15,7 @@ public class Client {
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("Wazuup!");
+            out.println("Wazzup!");
             String response = in.readLine();
             System.out.println(response);
         } catch (IOException e) {

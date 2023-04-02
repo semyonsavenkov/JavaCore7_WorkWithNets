5c382d58a9ffd85baac8d0e44d3dbd007216da01
semyonsavenkov <semyonsavenkov@gmail.com>
2023-04-02 22:41:01 +0300

    "try-catch" part added

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -9,14 +9,17 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         int port = 8085;
-        ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        System.out.println("New connection accepted");
-        final String name = in.readLine();
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ) {
+                System.out.println("New connection accepted");
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
+        }
     }
 
 }
